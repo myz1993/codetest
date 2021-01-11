5ea8d1d1996a70a89f3e183efa57b7f7102d1232
Lydia Zhang <leo.liu@vudoo.com>
2021-01-11 21:41:18 +1100

    add customized style

---
+++ frontend/src/components/app/style.ts
@@ -0,0 +1,22 @@
+const style = {
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+  icon: {
+    width: '18px',
+    maxHeight: '18px',
+    overflow: 'hidden',
+    margin: '0 7px -3px 0',
+  },
+  up: {
+    color: 'green',
+  },
+  down: {
+    color: 'red',
+  },
+};
+
+export default style;
