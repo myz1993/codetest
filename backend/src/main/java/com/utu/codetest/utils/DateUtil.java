70df5101314c6166268547a748fb30164eaec5b6
Lydia Zhang <leo.liu@vudoo.com>
2021-01-11 15:01:49 +1100

    add date calculate method

--- backend/src/main/java/com/utu/codetest/utils/DateUtil.java
+++ backend/src/main/java/com/utu/codetest/utils/DateUtil.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,4 +25,13 @@ public class DateUtil {
         return formatter.format(date);
     }
 
+    public Date calculate(Date date, Integer type, Integer days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(type, days);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
 }
