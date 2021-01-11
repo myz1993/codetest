a24eb764df1bd31b71cf8138b567ff86aad8a210
Lydia Zhang <leo.liu@vudoo.com>
2021-01-11 19:46:42 +1100

    create react app and init project

--- backend/src/main/java/com/utu/codetest/controllers/CryptoHistoryController.java
+++ backend/src/main/java/com/utu/codetest/controllers/CryptoHistoryController.java
@@ -4,6 +4,7 @@ import com.utu.codetest.dtos.CurrencySummaryDto;
 import com.utu.codetest.services.CryptoHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping(path = "/history")
 public class CryptoHistoryController {
     private final CryptoHistoryService service;
